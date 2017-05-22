86251d0d7b154a8624f22de601e0e1e5f3523520
Jan-Willem Taylor Parkins <0891769@hr.nl>
2017-05-22 11:39:04 +0200

    maze game
    maze game

---
+++ Maze Game/dist/js/canvas.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+    var img = new Image();
+    img.src = './images/level2.png'
+    ctx.drawImage(img,0,0,1300,740);
+}
