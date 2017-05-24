313c5e614f8d90d2895773ffb52295adc85af9ce
Jan-Willem Taylor Parkins <0891769@hr.nl>
2017-05-24 09:40:40 +0200

    img
    img

--- Maze Game/dist/js/canvas.js
+++ Maze Game/dist/js/canvas.js
@@ -4,14 +4,6 @@ window.onload = function(){
     var canvasWidth = window.innerWidth;
     var canvasHeight = window.innerHeight;
     var img = new Image();
-<<<<<<< HEAD
     img.src = './images/lv2.png'
-
-    ctx.drawImage(img,0,0,1137,656);
-=======
-    img.src = './images/level2.png'
     ctx.drawImage(img,0,0,1300,740);
-    //test
-    //test2
->>>>>>> origin/master
 }
